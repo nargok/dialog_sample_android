6bdd2817f7cc80c38417552c2b0d17aefe4f4f59
nargok <nargok@gmail.com>
2019-05-19 10:46:01 +0900

    日付選択ダイアログの作成

--- app/src/main/java/com/example/myalarmclock/Doialogs.kt
+++ app/src/main/java/com/example/myalarmclock/Doialogs.kt
@@ -1,10 +1,13 @@
 package com.example.myalarmclock
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.widget.DatePicker
 import androidx.fragment.app.DialogFragment
+import java.util.*
 
 class TimeAlartDialog: DialogFragment() {
     interface Listener {
@@ -33,5 +36,38 @@ class TimeAlartDialog: DialogFragment() {
             listener?.snooze()
         }
         return builder.create()
+    }
+}
+
+
+class DatePickerDialog: DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    interface OnDateSelectedListener {
+        fun onSelected(year: Int, month: Int, date: Int)
+    }
+
+    private var listener: OnDateSelectedListener? = null
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        when (context) {
+            // 呼んでるActivityをセットする
+            is OnDateSelectedListener -> listener = context
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // 現在の日付を初期値として設定する
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val date = c.get(Calendar.DAY_OF_MONTH)
+        // DatePickerDialogのインスタンスを返す
+        return DatePickerDialog(requireActivity(), this, year, month, date)
+    }
+
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        listener?.onSelected(year, month, dayOfMonth)
     }
 }
